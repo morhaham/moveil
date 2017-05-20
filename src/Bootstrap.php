db229887c7af87467804eda1714b550adda45d0b
Mor Haham <moring3071@gmail.com>
2017-05-20 21:21:01 +0200

    Http requests handling extension was added

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -22,6 +22,15 @@ if ($environment !== 'production') {
 }
 $whoops->register();
 
-throw new \Exception;
+$request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
+$response = new \Http\HttpResponse;
 
+
+foreach ($response->getHeaders() as $header) {
+	header($header, false);
+}
+
+$content = "<h1>Hello World</h1>";
+$response->setContent($content);
+echo $response->getContent();
 
